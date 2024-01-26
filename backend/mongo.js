d48afcec1d5cdc8d0f5cc706a4fdbb48d3a604fd
Mauricio <mauri1987@gmail.com>
2024-01-25 21:31:23 -0300

    uibuild

--- backend/mongo.js
+++ backend/mongo.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 
 
 const url =
-    `mongodb+srv://mqd-uy:${password}@cluster0.qstjepl.mongodb.net/notesApp?retryWrites=true&w=majority`
+  `mongodb+srv://mqd-uy:${password}@cluster0.qstjepl.mongodb.net/notesApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+  content: String,
+  important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
@@ -32,8 +32,8 @@ const Note = mongoose.model('Note', noteSchema)
 // })
 
 Note.find({}).then(result => {
-    result.forEach(note => {
-      console.log(note)
-    })
-    mongoose.connection.close()
+  result.forEach(note => {
+    console.log(note)
   })
+  mongoose.connection.close()
+})
